c325294a86393326de9eb08d870c986a46f4efed
pranshhhupg <guptaa.pranshu@gmail.com>
2026-04-15 02:52:39 +0530

    creating frontend of devTinder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/header'
+const App = function() {
+
+  return (
+    <>
+    <Header/>
+    <h1 className='text-2xl font-bold'>Starting our Project</h1>
+    </>
+  )
+}
+
+export default App
